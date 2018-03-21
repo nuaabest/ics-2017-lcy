3cd113cefca5ecbc9686aa14e822c68ab8ba6543
tracer-ics2017 <tracer@njuics.org>
2018-03-21 13:08:02 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  13:08:02 up 4 days,  7:41,  2 users,  load average: 0.00, 0.00, 0.00 a7a0bafea6698a914453cb4ecf83997dc34a87f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -67,7 +67,7 @@ static bool make_token(char *e) {
   regmatch_t pmatch;
 
   nr_token = 0;
-	//printf("%d",NR_REGEX);
+	printf("%ld",NR_REGEX);
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
