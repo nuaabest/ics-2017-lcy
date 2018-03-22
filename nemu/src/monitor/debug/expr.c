3c761e3edd7ff90b82fcc032237e27843ce62928
tracer-ics2017 <tracer@njuics.org>
2018-03-22 15:04:24 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  15:04:24 up 5 days,  9:38,  2 users,  load average: 0.04, 0.04, 0.01 16a39a3da3df416fa16cd4231d189bb182d725a8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -109,7 +109,7 @@ static bool make_token(char *e) {
           default:{
 						//	printf("%s",substr_start);
 							tokens[m].type=NUMBER;
-							strcpy(tokens[m].str,e);
+							strcpy(tokens[m].str,e+position-pmatch.rm_so);
 							break;
 					}
         }
