e884e547efc491e7747356f8bf96278de681fd0c
tracer-ics2017 <tracer@njuics.org>
2018-03-22 01:37:02 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  01:37:02 up 4 days, 20:10,  2 users,  load average: 0.05, 0.03, 0.00 1e21941233d228c3b1c254a754c3850876bb72d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -152,7 +152,7 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-//	printf("%d",m);
+	printf("%d",m);
 	int p=0,q=m-1;
 
   /* TODO: Insert codes to evaluate the expression. */
