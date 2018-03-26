ee6de2b0e3ab31350bb7dd5c242b99a697c4e9c1
tracer-ics2017 <tracer@njuics.org>
2018-03-26 11:04:31 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  11:04:31 up 9 days,  5:38,  2 users,  load average: 0.00, 0.00, 0.00 6d216699f9601be0d2130d96d83f28612a498b68

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -166,7 +166,7 @@ static bool make_token(char *e) {
 							unsigned int hex;
               int oct=0;
 						  tokens[m].type=HEX;
-							strncpy(need,substr_start-1,substr_len);
+							strncpy(need,substr_start+2,substr_len);
 							printf("%s\n",need);
 							sscanf(need,"%x",&hex);
               oct=hex;
