ba0c87f20f949b49bd786702a1c15b20530597d5
tracer-ics2017 <tracer@njuics.org>
2018-03-27 17:16:11 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  17:16:11 up 10 days, 11:50,  3 users,  load average: 0.02, 0.03, 0.00 3d82e071553ee3766684a8d9640fd592d38c1b2e

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -19,5 +19,36 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP *new_wp(){
+				if(free_==NULL) assert(0);
+				WP *point=free_;
+				free_=free_->next;
+				if(head==NULL) head=point;
+				else{
+								WP *point_=head;
+								while(point_->next!=NULL) point_=point_->next;
+								point_->next=point;
+				}
+				point->next=NULL;
+				return point;
+}
 
-
+void free_wp(WP *wp){
+				if(!(wp_pool <= wp && wp <=wp_pool+NR_WP)) assert(0);
+				WP *point_1=head;
+				WP *point_2=free_;
+				if(point_1==wp) head=wp->next;
+				else{
+								while(point_1->next!=wp) point_1=point_1->next;
+								point_1->next=NULL;
+				}
+				if(point_2==NULL){
+								free_=wp;
+								wp->next=NULL;
+				}
+				else{
+								while(point_2->next!=NULL) point_2=point_2->next;
+								point_2->next=wp;
+								wp->next=NULL;
+				}
+}
