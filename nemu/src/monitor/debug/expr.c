f757ec1002132622916f37dc1c2065b510b96f7c
tracer-ics2017 <tracer@njuics.org>
2018-03-28 16:23:43 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  16:23:43 up 11 days, 10:57,  3 users,  load average: 0.02, 0.02, 0.00 794db624a43bc6cbe855972aa539211144a761fc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -79,8 +79,8 @@ static bool make_token(char *e) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
-        Log("match rules[%d] = \"  %s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        //Log("match rules[%d] = \"  %s\" at position %d with len %d: %.*s",
+           // i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
@@ -323,7 +323,6 @@ int eval(int p,int q){
 											 }
 							 }
 				}
-				printf("%d\n",op);
         int val1=eval(p,op-1);
 			  int val2=eval(op+1,q);
 			  switch(tokens[op].type){
