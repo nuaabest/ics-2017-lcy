d5d8ee99f017e9c719c8b549465c472a6a0234cc
tracer-ics2017 <tracer@njuics.org>
2018-03-31 10:02:11 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  10:02:11 up 14 days,  4:36,  2 users,  load average: 0.00, 0.00, 0.00 b4e396366e3b433b6208b4b1b397d89fe70b2d51

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -232,7 +232,7 @@ int eval(int p,int q){
 		for(int count=p;count<=q;count++){
 				 num1[count]=atoi(tokens[count].str);
 		}
-		printf("%d  %d\n",p,q);
+		//printf("%d  %d\n",p,q);
 		int sta=10;
 		if(p>q){
          printf("Bad expression!\n");
@@ -321,7 +321,7 @@ int eval(int p,int q){
 															 sta=num1[count];
                                op=count;
 											 }
-											 printf("%d\n",op);
+											 //printf("%d\n",op);
 							 }
 				}
         int val1=eval(p,op-1);
