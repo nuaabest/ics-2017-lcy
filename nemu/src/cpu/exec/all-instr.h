44e83ff74844d4a690a8fef4b05cb55798cb39cd
tracer-ics2017 <tracer@njuics.org>
2018-04-17 08:12:43 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  08:12:43 up 31 days,  2:46,  3 users,  load average: 0.08, 0.02, 0.01 fa25d5ddd1756201b4292a3c71534d5e729702a9

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -3,8 +3,10 @@
 make_EHelper(mov);
 
 make_EHelper(operand_size);
+make_EHelper(sub);
 make_EHelper(push);
 make_EHelper(pop);
+make_EHelper(xor);
 make_EHelper(call);
 make_EHelper(ret);
 make_EHelper(inv);
