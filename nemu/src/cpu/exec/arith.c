affa0d067b3602fbcd0024976fb3b1779acd44f7
tracer-ics2017 <tracer@njuics.org>
2018-04-19 16:21:51 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  16:21:51 up 33 days, 10:55,  4 users,  load average: 0.22, 0.07, 0.02 e5b6f0d09b78598054e2fc038d12ef0a969840d5

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,16 +7,15 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-	printf("afasf\n");
   rtl_sub(&t2,&id_dest->val,&id_src->val);
 	rtl_sltu(&t3,&id_dest->val,&t2);
 
 	rtl_update_ZFSF(&t2,id_dest->width);
 
-	//rtl_sltu(&t0,&id_dest->val,&t2);
-	//rtl_or(&t0,&t3,&t0);
-	//rtl_set_CF(&t0);
-  printf("qfafas\n");
+	rtl_sltu(&t0,&id_dest->val,&t2);
+	rtl_or(&t0,&t3,&t0);
+	rtl_set_CF(&t0);
+  printf("1111\n");
 	rtl_xor(&t0,&id_dest->val,&id_src->val);
 	rtl_xor(&t1,&id_dest->val,&t2);
 	rtl_and(&t0,&t0,&t1);
