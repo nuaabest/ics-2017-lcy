25b901488869892eb5aa52727ac8100fbb43ba7d
tracer-ics2017 <tracer@njuics.org>
2018-04-24 14:10:37 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  14:10:37 up 38 days,  8:44,  4 users,  load average: 0.11, 0.03, 0.01 9bc11ebe1b6c78e555f62d19ff26bfa3de27f79e

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -42,6 +42,9 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 				*dest=(cpu.eflags.ZF||(cpu.eflags.SF!=cpu.eflags.OF));
 				break;
 		}
+		case CC_NE:{
+				*dest=(!cpu.eflags.ZF);
+		}
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
