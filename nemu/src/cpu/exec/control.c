ce940477daeb5a148f1316f363b6995a122a59d0
tracer-ics2017 <tracer@njuics.org>
2018-04-24 12:01:01 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  12:01:01 up 38 days,  6:34,  4 users,  load average: 0.00, 0.00, 0.00 af16131f7318d5fc22860eb1678c16ddf0e1fca4

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -12,7 +12,7 @@ make_EHelper(jcc) {
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
   decoding.is_jmp = t2;
-  printf("(%x\n)",decoding.is_jmp);
+  printf("(%x\n)",decoding.jmp_eip);
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
 }
 
