72cdbb1e0d009a98e990dc9b04b49e70eb08eb3a
tracer-ics2017 <tracer@njuics.org>
2018-04-24 11:06:12 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  11:06:12 up 38 days,  5:40,  4 users,  load average: 0.00, 0.00, 0.00 738b7e45b38e604af35001dff998892b40a8cb00

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -26,8 +26,12 @@ make_EHelper(xor) {
 }
 
 make_EHelper(or) {
-  TODO();
-
+  //TODO();
+	rtl_or(&t0,&id_dest->val,&id_src->val);
+	operand_write(id_dest,&t0);
+  rtl_update_ZFSF(&t0,id_dest->width);
+	rtl_set_CF(&tzero);
+	rtl_set_OF(&tzero);
   print_asm_template2(or);
 }
 
