7871b62b9d21b3abfbabcd5b6d7792541bf03f30
tracer-ics2017 <tracer@njuics.org>
2018-04-24 16:51:38 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  16:51:38 up 38 days, 11:25,  6 users,  load average: 0.00, 0.00, 0.00 ce2408972450a7129d3e2b431afd1ba06a9d430b

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -262,7 +262,7 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
-  decoding.jmp_eip = id_dest->simm + *eip;
+  decoding.jmp_eip = id_dest->simm + cpu.eip;
 	//printf("/%x/\n",decoding.jmp_eip);
 }
 
