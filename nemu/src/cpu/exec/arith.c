9bff0f7b177cdc080292b80b75c642049a0f8bbb
tracer-ics2017 <tracer@njuics.org>
2018-04-24 16:39:57 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  16:39:57 up 38 days, 11:13,  6 users,  load average: 0.00, 0.00, 0.00 eac863de3903f32d223237a77b6ae194def9f17e

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -21,11 +21,9 @@ make_EHelper(add) {
 
 make_EHelper(sub) {
   rtl_sub(&t2,&id_dest->val,&id_src->val);
-	//rtl_sltu(&t3,&id_dest->val,&t2);
 	rtl_update_ZFSF(&t2,id_dest->width);
 
 	rtl_sltu(&t0,&id_dest->val,&t2);
-	rtl_or(&t0,&t3,&t0);
 	rtl_set_CF(&t0);
  
 	rtl_xor(&t0,&id_dest->val,&id_src->val);
@@ -40,11 +38,9 @@ make_EHelper(sub) {
 make_EHelper(cmp) {
   //TODO();
   rtl_sub(&t2,&id_dest->val,&id_src->val);
-	//rtl_sltu(&t3,&id_dest->val,&t2);
 	rtl_update_ZFSF(&t2,id_dest->width);
 
 	rtl_sltu(&t0,&id_dest->val,&t2);
-	//rtl_or(&t0,&t3,&t0);
 	rtl_set_CF(&t0);
 
   rtl_xor(&t0,&id_dest->val,&id_src->val);
