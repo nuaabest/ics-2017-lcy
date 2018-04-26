f731126e0bda672eb6a1b240fcc0f863a9746136
tracer-ics2017 <tracer@njuics.org>
2018-04-26 04:55:34 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  04:55:34 up 39 days, 23:29,  3 users,  load average: 0.00, 0.00, 0.00 25dc1a61bf881efe34f4d89ae0f3b97ffed8d231

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -2,9 +2,9 @@
 
 make_EHelper(add) {
   //TODO();
-   rtl_add(&t2,&id_dest->val,&id_src->val);
-   rtl_sltu(&t3,&t2,&id_dest->val);
-	 rtl_update_ZFSF(&t2,id_dest->width);
+  rtl_add(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+	rtl_update_ZFSF(&t2,id_dest->width);
 
 	rtl_sltu(&t0,&id_dest->val,&t2);
 	rtl_or(&t0,&t3,&t0);
