9962aac62b283cd329dbc02de5eb9adea9e7dab9
tracer-ics2017 <tracer@njuics.org>
2018-04-27 11:16:05 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  11:16:05 up 41 days,  5:49,  3 users,  load average: 0.03, 0.01, 0.00 9f384a8c9429922d886089488363933f39e655e6

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -140,7 +140,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
 	*dest=(*src1<<(32-width*8));
-	*dest=(*dest>>31);
+	*dest=(*dest>>(32-width*8));
 	//printf("%d\n",*dest);
 }
 
