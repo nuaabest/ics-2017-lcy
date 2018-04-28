68d075e9aa09627e121996bea55ebecc74f95ff7
tracer-ics2017 <tracer@njuics.org>
2018-04-28 11:59:32 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  11:59:32 up 42 days,  6:33,  4 users,  load average: 0.11, 0.03, 0.01 529afa2a68ed3ea9aee5ae2e8a40bb9f740062

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -142,9 +142,10 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   printf("eax2:0x%08x  %d\n\n",*src1,width);
 	*dest=(*src1<<(32-width*8));
 	printf("dest1:0x%08x\n",*dest);
-	*dest=(*dest>>(31));
+	int flag1=*dest>>31;
+	*dest=(*dest>>(32-width*8));
 	printf("dest2:0x%08x\n",*dest);
-	int flag=*dest&0x1;
+	int flag=flag1&0x1;
 	if(width==1){
 					if(flag==1) *dest=*dest|0xffffff00;
 	}
