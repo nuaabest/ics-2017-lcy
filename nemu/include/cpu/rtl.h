79ad49c261f8ed0fc7028e46c3c4cac9a8426398
tracer-ics2017 <tracer@njuics.org>
2018-04-28 11:29:31 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  11:29:31 up 42 days,  6:03,  4 users,  load average: 0.12, 0.03, 0.01 3ae25deb28e556aa2a5bc227cf4de8397f579948

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -139,6 +139,7 @@ static inline void rtl_not(rtlreg_t* dest) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
+  printf("eax2:0x%08x  %d\n\n",cpu.eax,width);
 	*dest=(*src1<<(32-width*8));
 	*dest=(*dest>>(32-width*8));
 	//printf("%d\n",*dest);
