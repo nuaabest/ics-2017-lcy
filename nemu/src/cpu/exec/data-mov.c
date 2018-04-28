69d6191cd93a23a783df3b4f470a89a9d820c509
tracer-ics2017 <tracer@njuics.org>
2018-04-28 11:08:54 +0000

    >  compile 161620118 nuaa_lcy Linux 968faef3ae3e 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  11:08:54 up 42 days,  5:42,  3 users,  load average: 0.00, 0.00, 0.00 652849f1cbc677df2b1dc5a85c00fb5ac87d83b0

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -71,8 +71,10 @@ make_EHelper(cwtl) {
 }
 
 make_EHelper(movsx) {
+	printf("eax1:%d\n",cpu.eax);
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_sext(&t2, &id_src->val, id_src->width);
+	printf("eax2:%d\n",cpu.eax);
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);
 }
